a13849da33b1001f55caae015c43ad684740e59b
KyloP311 <palalaykylo43@gmail.com>
2025-02-25 21:35:45 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -7,7 +7,7 @@ public class activity1 {
         //EXAMPLE:
 
         System.out.println("BENOYA, JHOVENSON D. - BSIT-2I");
-
+        System.out.println("KYLO, PALALAY D. - BSIT-2J");
 
         System.out.println("JHONNY B. MAPULA - BSIT-2J");
 
